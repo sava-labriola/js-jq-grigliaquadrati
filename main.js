c9bcc4fc56e70a04862b11f97851d31817e698ea
sava-labriola <labriolasava00@gmail.com>
2020-05-07 16:30:32 +0200

    completato con entrambi i bonus

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Creare in HTML una griglia di 5x5 quadrati vuoti.
+// Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
+// BONUS 1: generare la griglia con jQuery
+// BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+
 var numero_quadrati = 25;
 for (var i = 0; i < numero_quadrati; i++) {
     $('.griglia').append("<div class='quadrato'><p></p></div>");
